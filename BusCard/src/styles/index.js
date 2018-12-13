1b9155865b709d73f9e5b777c1e6a6c7112ae847
GabrielScalici <gabrielhc.scalici@gmail.com>
2018-12-13 08:40:15 -0200

    adicionado configs

---
+++ BusCard/src/styles/index.js
@@ -0,0 +1,14 @@
+/**
+ * FRM - Esiga
+ * 
+ * Autor: Gabriel Henrique Campos Scalici
+ * 
+ * Função Tela : Todos os arquivos dessa pasta tem por finalidade padronizar todas as
+ *               interfaces e métricas do aplicativo
+**/
+
+import colors from './colors';
+import metrics from './metrics';
+import font from './font';
+
+export { colors, font, metrics };
